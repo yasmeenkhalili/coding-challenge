5149925b3e550e64834c9786ad07d5e7fe90e5b8
yasmeenkhalili <jasmina201132@yahoo.com>
2021-03-30 13:31:47 +0300

    the coding challenge

---
+++ challenge-1-solution.js
@@ -0,0 +1,7 @@
+let numberArray = [9, 16, 100, 75, 1000];
+function squareRoothOfNumber() {
+  for (let i = 0; i < numberArray.length; i++) {
+    let squareRoot = Math.sqrt(numberArray[i]);
+    console.log(squareRoot);
+  }
+}squareRoothOfNumber();
---
+++ challenge-2-solution.js
@@ -0,0 +1,15 @@
+let number1=parseInt(prompt('Enter your first number')) ;
+let number2=parseInt(prompt('Enter your second number'));
+let number3=parseInt(prompt('Enter your third number'));
+let number4=parseInt(prompt('Enter your forth number'));
+let number5=parseInt(prompt('Enter your fifth number'));
+
+let theMaximum=Math.max(number1,number2,number3,number4,number5);
+alert('The maximum number is  '+ theMaximum);
+console.log(theMaximum);
+
+
+let theMinimum=Math.min(number1,number2,number3,number4,number5);
+alert('The minimum number is  '+ theMinimum);
+console.log(theMinimum);
+
